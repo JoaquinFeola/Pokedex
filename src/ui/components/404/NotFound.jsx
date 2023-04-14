5014736020c211abd7e887184fe0e0d30a107ba0
Joaquin Feola <joacoarielfeola@gmail.com>
2023-04-14 10:50:42 -0300

    cambie directorios

--- src/ui/components/404/NotFound.jsx
+++ src/ui/components/404/NotFound.jsx
@@ -4,12 +4,10 @@ export const NotFound = () => {
   return (
     <div className='container content-page-container d-flex justify-content-center '>
         <img 
-            src="/public/not_found.svg"
+            src="/assets/not_found.svg"
             className='card-img w-50' 
-            alt="" 
+            alt="Page not found -- like image" 
         />
-        {/* <h2 className='display-1'>404</h2>
-        <p className='display-4'>This page not found</p> */}
     </div>
   )
 }
