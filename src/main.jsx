2bb71ea0a93a68f21f48f26b3bcbf8f2f4119b6a
Joaquin Feola <joacoarielfeola@gmail.com>
2023-04-18 11:35:47 -0300

    agregue el netlify.toml

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { PokedexProvider } from './context/PokedexProvider';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import 'animate.css'
+import 'animate.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <PokedexProvider>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </PokedexProvider>
-  </HashRouter>
+  </BrowserRouter>
 
 )
